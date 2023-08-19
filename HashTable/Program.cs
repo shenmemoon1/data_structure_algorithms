d5677e22d7e1d9fa41667c903fd518cfc753fd51
billlims <davidlinxf@gmail.com>
2023-08-19 17:28:36 +1000

    update hashtable

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -15,12 +15,93 @@ namespace HashTable
         }
     }
 
+
+    //基于数组实现哈希表
     public class HashTable
     {
+        private List<Pair?> buckets;
+        public HashTable()
+        {
+            //initial 100 buckets
+            buckets = new();
+            for(int i = 0; i<100; i++)
+            {
+                buckets.Add(null);
+            }
+        }
+
+        //hash fucntion
         public int HashF(int key)
         {
-            return;
+            int index = key % 100;
+            return index;
         }
+
+        //添加操作
+        public void put(int key, string val)
+        {
+            int index = HashF(key);
+            Pair pair = new(key, val);
+            buckets[index] = pair;
+        }
+
+        //查询操作
+        public string? get(int key)
+        {
+            //获取在List的index
+            int index = HashF(key);
+            Pair? pair = buckets[index];
+            if (pair == null) return null;
+            return pair.value;
+        }
+
+        //删除操作
+        public void remove(int key)
+        {
+            int index = HashF(key);
+            buckets[index] = null;
+        }
+
+        //获取所有键
+        public List<int> keySet()
+        {
+            List<int> keySet = new();
+            foreach(Pair? pair in buckets)
+            {
+                if(pair != null)
+                {
+                    keySet.Add(pair.key);
+                }
+            }
+
+            return keySet;
+        }
+
+        //获取所有的value set
+        public List<string> valueSet()
+        {
+            List<string> valueSet = new();
+            foreach (Pair? pair in buckets)
+            {
+                if (pair != null)
+                {
+                    valueSet.Add(pair.value);
+                }
+            }
+
+            return valueSet;
+        }
+
+        //打印哈希表
+        public void print()
+        {
+            foreach(Pair? pair in buckets)
+            {
+                if(pair !=null)
+                    Console.WriteLine($"{pair.key} -> {pair.value}");
+            }
+        }
+
 
 
     }
@@ -30,7 +111,11 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            
+            HashTable hashTable = new HashTable();
+            hashTable.put(20336, "David");
+            hashTable.put(12836, "Kevin");
+            hashTable.put(48693, "Lilly");
+            hashTable.print();
         }
     }
 }
