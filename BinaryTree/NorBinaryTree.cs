5364c74f249ca6debba1036a138753d035adb466
billlims <davidlinxf@gmail.com>
2023-08-20 12:02:45 +1000

    基本的二叉树操作，树的基础结构和插入删除操作

---
+++ BinaryTree/NorBinaryTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class TreeNode
+    {
+        public int val; //节点值
+        public TreeNode? left; //左子节点
+        public TreeNode? right; //右子节点
+        public TreeNode(int value) => val = value;
+    }
+
+
+    /*class NorBinaryTree
+    {
+        
+    }*/
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BinaryTree;
+
+namespace BinaryTree
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode treeNode1 = new TreeNode(33);
+            TreeNode treeNode2 = new TreeNode(66);
+            TreeNode treeNode3 = new TreeNode(89);
+            TreeNode treeNode4 = new TreeNode(59);
+
+            treeNode1.left = treeNode2;
+            treeNode1.right = treeNode3;
+            treeNode2.left = treeNode4;
+
+            TreeNode p = new(0);
+            treeNode1.left = p;
+            p.left = treeNode2;
+
+
+        }
+    }
+}
