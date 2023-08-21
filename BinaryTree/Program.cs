327fd924ab2c83cc9c73ddd1fa6fc11b1da389a8
billlims <davidlinxf@gmail.com>
2023-08-21 17:45:23 +1000

    pre-order traveral

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using BinaryTree;
 
 namespace BinaryTree
@@ -6,20 +7,64 @@ namespace BinaryTree
 
     class Program
     {
+        public static List<int> list2 = new List<int>();
+        //层序遍历 level-order traversal
+        public static List<int> levelOrder(TreeNode root)
+        {
+            //初始化队列 加入根节点
+            Queue<TreeNode> que = new();
+            que.Enqueue(root);
+            //初始化一个列表， 用于保存遍历序列
+            List<int> list = new();
+            while (que.Count != 0)
+            {
+                TreeNode node = que.Dequeue(); //出列
+                list.Add(node.val); //保存节点值
+                if (node.left != null)
+                {
+                    que.Enqueue(node.left); //左节点入列
+                }
+                if (node.right != null)
+                {
+                    que.Enqueue(node.right); //右节点入列
+                }
+            }
+
+            return list;
+        }
+        //前序遍历
+        public static void preOrder(TreeNode root)
+        {
+            if (root == null) return;
+
+            //访问优先级 root->left->right
+            list2.Add(root.val);
+            //looping left
+            preOrder(root.left);
+            //looping right
+            preOrder(root.right);
+
+           
+        }
+
         static void Main(string[] args)
         {
-            TreeNode treeNode1 = new TreeNode(33);
-            TreeNode treeNode2 = new TreeNode(66);
-            TreeNode treeNode3 = new TreeNode(89);
-            TreeNode treeNode4 = new TreeNode(59);
+            TreeNode treeNode1 = new TreeNode(1);
+            TreeNode treeNode2 = new TreeNode(2);
+            TreeNode treeNode3 = new TreeNode(3);
+            TreeNode treeNode4 = new TreeNode(4);
+            TreeNode treeNode5 = new TreeNode(5);
 
             treeNode1.left = treeNode2;
             treeNode1.right = treeNode3;
             treeNode2.left = treeNode4;
+            treeNode2.right = treeNode5;
 
-            TreeNode p = new(0);
-            treeNode1.left = p;
-            p.left = treeNode2;
+            preOrder(treeNode1);
+            foreach (int val in list2)
+            {
+                Console.Write($"{val}-");
+            }
 
 
         }
