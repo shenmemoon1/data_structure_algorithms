ea6ef4ac011ab2df51cf8ea3ccc8bc3b075529b1
billlims <davidlinxf@gmail.com>
2023-08-24 17:27:10 +1000

    左右旋转树解构

--- AVLTree/Program.cs
+++ AVLTree/Program.cs
@@ -33,15 +33,32 @@ namespace AVLTree
         public static TreeNode? rightRotate(TreeNode? node)
         {
             TreeNode? child = node.left;
-            TreeNode? grandChild = child?.right;
+            TreeNode? grandChild = child.right;
             //以child作为原点 将node向右旋转
             child.right = node;
             node.left = grandChild;
             //更新节点高度
             updateHeight(node);
             updateHeight(child);
+
             return child;
         }
+
+        /* 左旋操作 */
+        static TreeNode? leftRotate(TreeNode? node)
+        {
+            TreeNode? child = node.right;
+            TreeNode? grandChild = child.left;
+            // 以 child 为原点，将 node 向左旋转
+            child.left = node;
+            node.right = grandChild;
+            // 更新节点高度
+            updateHeight(node);
+            updateHeight(child);
+            // 返回旋转后子树的根节点
+            return child;
+        }
+
 
         static void Main(string[] args)
         {
@@ -50,11 +67,34 @@ namespace AVLTree
             TreeNode t3 = new TreeNode(5);
             TreeNode t4 = new TreeNode(1);
             TreeNode t5 = new TreeNode(3);
+            TreeNode t6 = new TreeNode(6);
+            TreeNode t7 = new TreeNode(8);
 
             t1.left = t2;
             t1.right = t3;
             t2.left = t4;
             t2.right = t5;
+            t3.right = t6;
+            t6.right = t7;
+
+            leftRotate(t3);
+
+            //解释
+            /*
+             * 在这里设置了child 和 grandchil来保存子孙节点
+             首先 5-》 6-》8 右侧大于左部
+            进行左旋转
+                    6
+                    |
+                5 <- -> 8
+            6作为根
+            5变它的左节点
+            8保持
+            但是还需要知道6之前是否有左节点 如果有就放在5 的右侧 没有就返回
+            所有变成 5
+                    |
+            null <- -> null
+             */
 
         }
     }
