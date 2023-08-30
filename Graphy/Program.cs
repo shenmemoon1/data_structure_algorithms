653a28dbdafd1066ebca37fe2cc5c2c411858b6b
billlims <davidlinxf@gmail.com>
2023-08-30 20:55:37 +1000

    data structure graphy

---
+++ Graphy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graphy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Graphy!");
+        }
+    }
+}
--- Heap/FindKValueHeap.cs
+++ Heap/FindKValueHeap.cs
@@ -36,9 +36,7 @@ namespace Heap
             }
             return kList;
 
-
         }
-
 
     }
 }
