9d62657cf5cfbee9d945e516e361f4e342686410
billlims <davidlinxf@gmail.com>
2023-08-30 18:44:05 +1000

    find kvalue solution one

--- Heap/Program.cs
+++ Heap/Program.cs
@@ -8,11 +8,18 @@ namespace Heap
         static void Main(string[] args)
         {
 
-            int[] arr = { 2, 43, 5, 67, 8, 9, 0, 66 };
+            /*int[] arr = { 2, 43, 5, 67, 8, 9, 0, 66 };
             MaxHeap maxHeapInstance = new MaxHeap(arr);
             int a = maxHeapInstance.peek();
             List<int> b = maxHeapInstance.print();
             foreach (int item in b)
+            {
+                Console.WriteLine(item);
+            }*/
+            List<int> l = new List<int> { 1, 4, 65, 787, 9, 4 };
+            FindKvalue f = new FindKvalue(l);
+            List<int> a = f.findkMax(3);
+            foreach (int item in a)
             {
                 Console.WriteLine(item);
             }
---
+++ Heap/findKvalue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    public class FindKvalue
+    {
+        List<int> Arr;
+        //method 1 => i traveral
+        public FindKvalue(List<int> arr)
+        {
+            Arr = arr;
+        }
+
+        public List<int> findkMax(int k)
+        {
+            List<int> list = new();
+            int max = 0;
+            int len = 0;
+            while (len < Arr.Count)
+            {
+                for (int i = len + 1; i < Arr.Count; i++)
+                {
+                    if (Arr[max] < Arr[i])
+                    {
+                        max = i;
+                       
+
+                        // 3
+                    }
+
+                    continue;
+                }
+
+                if (list.Count <= 3)
+                {
+                    list.Add(Arr[max]);
+                    Arr.RemoveAt(max);
+                }
+                else
+                {
+                    break;
+                }
+                len++;
+            }
+            return list;
+            
+            
+        }
+    }
+}
