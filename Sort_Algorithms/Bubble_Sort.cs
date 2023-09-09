a4e826a324519bf59a7b76b7c42339ecd91a49a4
billlims <davidlinxf@gmail.com>
2023-09-09 23:04:36 +1000

    Bubble sort implement

---
+++ Sort_Algorithms/Bubble_Sort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Algorithms
+{
+    class Bubble_Sort
+    {
+        public void bubble_sort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        (nums[i], nums[j]) = (nums[j], nums[i]);
+                    }
+                }
+            }
+        }
+    }
+}
--- Sort_Algorithms/Program.cs
+++ Sort_Algorithms/Program.cs
@@ -9,7 +9,11 @@ namespace Sort_Algorithms
             int[] nums = { 4, 1, 3, 1, 5, 2 };
             //SELECT SORT
             Selection_Sort selection_ = new();
-            selection_.selectionSort(nums);
+            //selection_.selectionSort(nums);
+
+            //Bubble Sort
+            Bubble_Sort bubble_ = new();
+            bubble_.bubble_sort(nums);
         }
     }
 }
