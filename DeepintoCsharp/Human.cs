0f52281ba36b4ed6b063af3269cf18233f66b11b
billlims <davidlinxf@gmail.com>
2023-09-13 23:25:25 +1000

    deepintocsharp

---
+++ DeepintoCsharp/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepintoCsharp
+{
+    class Human
+    {
+        //字段
+        public string name { get; set ;}
+        public static int counter=0;
+        public readonly string color;
+
+        //静态方法 直接可以调用
+        public static void SayHi(string name)
+        {
+            Console.WriteLine($"hi {name}");
+        }
+        //构造器
+        public Human(string n)
+        {
+            name = n;
+            counter++;
+            //只能一次 readonly属性
+            color = "blue";
+        }
+    }
+}
---
+++ DeepintoCsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DeepintoCsharp
+{
+    class Program
+    {
+        /*
+         * "class"（类）是一种用于创建对象的蓝图或模板。
+         * 飞行员 <--实例-- 人类
+         * 宝马 <--实例--汽车
+         */
+        static void Main(string[] args)
+        {
+            //实例了一个对象
+            //引用变量
+            Human pilot = new("Kevin");
+            //实例2
+            Human p2 = new("lily");
+            //每次实例一个就添加一个counter
+            Console.WriteLine(Human.counter);
+        }
+    }
+}
