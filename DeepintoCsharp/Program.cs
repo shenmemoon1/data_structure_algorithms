5a52c7051ddfec4165e5643335e9a6164db3c224
billlims <davidlinxf@gmail.com>
2023-09-14 19:19:03 +1000

    value, ref,out, params, naming param and this extension param

--- DeepintoCsharp/Program.cs
+++ DeepintoCsharp/Program.cs
@@ -20,6 +20,7 @@ namespace DeepintoCsharp
             //Console.WriteLine(Human.counter);
 
             //索引器
+            Console.WriteLine("--------------------------索引器---------------------------");
             Student s1 = new();
             s1["Math"] = 90;
             var mathScroe = s1["Math"];
@@ -29,7 +30,7 @@ namespace DeepintoCsharp
 
             Console.WriteLine("--------------------------值类型---------------------------");
             int x = 100;
-            Para para = new(s1);
+            Para para = new(ref s1);
             //实参传递给形参（副本）
             para.AddOne(x);
             //x 并没有修改
@@ -38,6 +39,124 @@ namespace DeepintoCsharp
             Console.WriteLine(s1.Name);
             Console.WriteLine(s1.GetHashCode());
 
+
+            Console.WriteLine("--------------------------引用参数---------------------------");
+            int y = 200;
+            para.AddTwo(ref y);
+            Console.WriteLine(y);
+
+            Console.WriteLine("--------------------------输用参数---------------------------");
+            //例子
+           /* Console.WriteLine("input a double value: ");
+            string arg1 = Console.ReadLine();
+            double z = 0;
+
+            // 使用 double.TryParse 尝试将用户输入的字符串转换为 double 类型
+            bool b1 = double.TryParse(arg1, out z);
+
+            if (b1 == false)
+            {
+                Console.WriteLine("input error");
+            }
+
+            Console.WriteLine("input a double value: ");
+            string arg2 = Console.ReadLine();
+            double j = 0;
+
+            // 使用 double.TryParse 尝试将用户输入的字符串转换为 double 类型
+            bool b2 = double.TryParse(arg1, out j);
+
+            if (b1 == false)
+            {
+                Console.WriteLine("input error");
+            }
+            double q = z + j;
+
+            Console.WriteLine("{0} + {1} = {2}", z, j, q);*/
+
+            Console.WriteLine("-----------------------实现double.parse-------------------------");
+            double o = 0;
+            bool aa = DoubleParse.TryParser("232", out o);
+            Console.WriteLine(o);
+
+            Console.WriteLine("-----------------------数组参数-------------------------");
+            //如果没有使用params 还需要声明一个数组传递进去 params放于参数最后一个
+            //使用params 直接传递数组 就可以 内部会转换成数组
+            int res =calculateSum(1, 2, 3, 4, 5);
+            Console.WriteLine(res);
+
+            //另外一个string的数组参数例子
+            string nameList = "tom,sdfsd.fgfdg/spdjfsd";
+            string[] strList= nameList.Split(',', '.', '/');
+            foreach (string item in strList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------------具名调用-------------------------");
+            //具名调用 就是方法使用参数 不需要按照顺序
+            //不具名例子
+            printInfo("sfj", 22);
+            //具名参数
+            printInfo(age: 33, name: "sdfsd");
+
+            Console.WriteLine("-----------------------可选参数-------------------------");
+            printInfo(name:"fsd");
+
+            Console.WriteLine("-----------------------扩张方法 this-------------------------");
+            //实现了一个round扩张方法
+            double pi = 3.14159;
+            //必须添加this 在方法的参数里面 才可以使用扩张方法
+            double yy = pi.Round(4);
+            Console.WriteLine(yy);
+        }
+
+        static void printInfo(string name, int age = 22)
+        {
+            Console.WriteLine("name {0}, age {1}", name, age);
+        }
+
+        //使用params 就不需要在调用之前声明一个数组
+        static int calculateSum(params int[] array)
+        {
+            int result = 0;
+            foreach (int  item in array)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+
+
+        class DoubleParse
+        {
+            //自己写一个tryparse
+            /// <summary>
+            /// 输入值 返回 bool result 如果成功 赋值给外部
+            /// </summary>
+            public static bool TryParser(string value, out double result)
+            {
+                try
+                {
+                    result = double.Parse(value);
+                    return true;
+                }
+                catch
+                {
+                    result = 0;
+                    return false;
+                }
+            }
+        }
+    }
+
+    //必须静态类
+    static class DoubleExtension
+    {
+        public static double Round(this double input, int digits)
+        {
+            double res = Math.Round(input, digits);
+            return res;
         }
     }
 }
--- DeepintoCsharp/para.cs
+++ DeepintoCsharp/para.cs
@@ -14,14 +14,24 @@ namespace DeepintoCsharp
          * a => 0x3436546(地址)
          *           /\
          * a ----| 直接指向内存，所以当ref值修改了 原来的值就修改了
-         * 参数修饰符 
+         * 引用参数 
          * ref 要初始化 out不需要
          * ref 参数（Reference Parameter）： 使用 ref 修饰符传递参数时，方法接收的是参数的引用，而不是值的副本。这允许在方法内部修改参数的值，并且这些修改会影响原始参数的值。
          * out 参数（Output Parameter）： 使用 out 修饰符传递参数时，方法接收的是参数的引用，并且方法负责为该参数赋值。在方法内部，必须确保为 out 参数分配一个值。
+         * 
+         * ***引用类型的引用参数****
+         * 第一次传尽力啊一个student的实例 这个本身是有一个内存地址的引用类型
+         * 但是在构造器内部从新创建了一个stu 这个使用修改了stu的name 那么最终会修改外部的stu
+         * 
+         * 
+         * 输出参数 out 不需要明确赋值
          */
-        public Para(Student stu)
+
+        public Para(ref Student stu)
         {
-            stu.Name = "Kevin";
+            /*stu.Name = "Kevin";*/
+            stu = new Student();
+            stu.Name = "Tim";
             Console.WriteLine(stu.GetHashCode());
         }
 
@@ -30,5 +40,17 @@ namespace DeepintoCsharp
             x += 1;
             Console.WriteLine(x);
         }
+
+        /// <summary>
+        /// ref 修改参数的值 不再是复制副本
+        /// </summary>
+        /// <param name="x"></param>
+        public void AddTwo(ref int x)
+        {
+            x += 2;
+            Console.WriteLine(x);
+        }
+
+
     }
 }
