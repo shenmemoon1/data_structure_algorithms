06446de5998a7fff2434300f0d5f5b90a6082de1
billlims <davidlinxf@gmail.com>
2023-09-20 19:09:10 +1000

    deeper Reflection

---
+++ DiveIntoReflection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiveIntoReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
