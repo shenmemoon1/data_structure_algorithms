cf7771584320f6b0cc8f4939f9f396d37d245037
billlims <davidlinxf@gmail.com>
2023-09-21 00:38:47 +1000

    dive into generic

---
+++ GenericDiveInto/GenExample1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDiveInto
+{
+    class GenExample1
+    {
+    }
+
+    //例子1 为什么要使用泛型
+    class Apple
+    {
+        public string Color { get; set; }
+    }
+
+    class Book
+    {
+        public string  Name { get; set; }
+    }
+
+    class Box
+    {
+        /*public Apple apple { get; set; }
+
+        public void AppleBox() 
+        {
+            Console.WriteLine($"{apple.Color}");
+            Console.WriteLine("apple box");
+
+        }*/
+
+        //使用另外的方法
+        public Object Cargo { get; set; }
+    }
+
+    /*class Box2
+    {
+        public Book book { get; set; }
+
+        public void BookBox()
+        {
+            Console.WriteLine($"{book.Name}");
+            Console.WriteLine("book box");
+
+        }
+    }*/
+
+
+    /*
+     * 上面两种不管怎么弄都很麻烦
+     * 第一总就是给不同的物品什么一个box类 
+     * 另外一个是直接使用object 然后强行转换类型
+     * 都不理想 接下来使用泛型
+     */
+    class Box<T>
+    {
+        public T Cargo { get; set; }
+    }
+
+    //例子2 泛型接口
+    interface IUnique<T>
+    {
+        T Id { get; set; }
+    }
+
+    //如果这个类使用泛型接口 那种这个类也是泛型类
+    //如下
+    class Student<T> : IUnique<T>
+    {
+        public T Id { get; set; }
+    }
+
+    //第二种 特化之后的类
+    //已经特化不需要
+    class Studnet2 : IUnique<ulong>
+    {
+        public ulong Id { get; set; }
+    }
+
+   
+}
---
+++ GenericDiveInto/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericDiveInto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //不理性的两种方式
+            /*Apple apple = new Apple() { Color = "red"};
+            Box box = new Box() { Cargo = apple };
+            Book book = new() { Name = "three hand shake"};
+            Console.WriteLine((box.Cargo as Apple)?.Color);*/
+            //Box2 box2 = new() { book = book };
+            //output
+            //box.AppleBox();
+            //box2.BookBox();
+
+            //泛型
+            //解决了类型转换和成员膨胀问题
+            Apple apple = new() { Color = "red" };
+            Book book = new() { Name = "three hand shake" };
+            Box<Apple> box = new() { Cargo = apple };
+            Box<Book> box2 = new() { Cargo = book };
+            Console.WriteLine(box.Cargo.Color);
+            Console.WriteLine(box2.Cargo.Name);
+
+            Console.WriteLine("---------------泛型类-------------------");
+            Student<int> student = new() { Id = 32423 };
+            Console.WriteLine(student.Id);
+
+            IList<int> list = new List<int>();
+            IDictionary<string, int> keyValues = new Dictionary<string, int>();
+
+            //Sum(1, 2);
+
+            Console.WriteLine("============泛型委托==================");
+            Action<string> action = Say;
+            action.Invoke("Bill");
+
+            Func<int, int, int> func = Add;
+            Console.WriteLine(func.Invoke(100, 300)) ;
+        }
+
+        //泛型委托
+        //action
+        public static void Say(string name)
+        {
+            Console.WriteLine("hello {0}",name);
+        }
+        //func
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        //写一个泛型方法
+       /* public static T Sum<T>(T a, T b)
+        {
+            dynamic x = a;
+            dynamic y = b;
+            dynamic z = x + y;
+            return z;
+        }*/
+    }
+}
