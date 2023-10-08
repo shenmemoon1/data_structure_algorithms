0e6a3eb2ae2f6f0e3d4a51a8f4e69c8da0705cef
billlims <davidlinxf@gmail.com>
2023-10-08 23:41:34 +1100

    solution for climb stair dp

---
+++ Danamic programming/DPclimbStair_dfs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danamic_programming
+{
+    class DPclimbStair_dfs
+    {
+        //爬楼梯搜索
+        public static int ClimbStairDFS(int n)
+        {
+            //传入n
+            return Dfs(n);
+        }
+
+        private static int Dfs(int i)
+        {
+            //中止条件
+            if (i == 1 || i ==2)
+            {
+                return i;
+            }
+
+            //最后的阶梯等于它的前两个阶梯的总和
+            return Dfs(i - 1) + Dfs(i - 2);
+        }
+    }
+}
--- Danamic programming/Program.cs
+++ Danamic programming/Program.cs
@@ -6,7 +6,10 @@ namespace Danamic_programming
     {
         static void Main(string[] args)
         {
-
+            //这个是使用回溯
+            Console.WriteLine(DPClimbStairs.ClimbStairBacktrack(6));
+            //DP
+            Console.WriteLine(DPclimbStair_dfs.ClimbStairDFS(6));
         }
     }
 }
