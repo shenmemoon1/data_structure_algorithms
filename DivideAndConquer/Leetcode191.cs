342b20f799bb329d5f85e991a6f33f695e2c4175
billlims <davidlinxf@gmail.com>
2023-10-13 02:35:39 +1100

    solution 2

--- DivideAndConquer/Leetcode191.cs
+++ DivideAndConquer/Leetcode191.cs
@@ -9,19 +9,43 @@ namespace DivideAndConquer
     class Leetcode191
     {
         //solution one
-        public int HammingWeight(uint n)
+        public static int HammingWeight(uint n)
         {
             int count = 0; // counter 1
                            //0 >> to 0 stop loop
             while (n > 0)
             {
                 count += (int)(n & 1);
-                n = n >> 1;
+                n >>= 1;
             }
 
             return count;
         }
 
         //solution two(using divide and conquer)
+
+        public static int HammingWeight2(uint n)
+        {
+            return CountOne(0, 32, n);
+        }
+
+        private static int CountOne(int left, int right, uint n)
+        {
+            // 中止条件
+            if (left >= right || n == 0)
+            {
+                return (int)(n & 1); // 返回当前位是否为1
+            }
+
+            int halfSize = (left + right) / 2;
+            uint leftHalf = n >> halfSize;
+            uint rightHalf = n & ((1U << halfSize) - 1);
+
+            int leftCount = CountOne(left, halfSize, leftHalf);
+            int rightCount = CountOne(halfSize, right, rightHalf);
+
+            return leftCount + rightCount;
+        }
+
     }
 }
