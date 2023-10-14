2cd7caa814bc8ccafded53ab901c72d0b9419c80
billlims <davidlinxf@gmail.com>
2023-10-14 16:31:41 +1100

    Leetcode21(using dc)

---
+++ DivideAndConquer/Leetcode21MergeTwoSortArr.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideAndConquer
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    class Leetcode21MergeTwoSortArr
+    {
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            //merge
+            if (list1.val < list2.val)
+            {
+                list1.next = MergeTwoLists(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = MergeTwoLists(list1, list2.next);
+                return list2;
+            }
+
+        }
+    }
+}
