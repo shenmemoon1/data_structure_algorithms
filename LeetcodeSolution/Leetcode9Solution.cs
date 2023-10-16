f527ab7dd9581b98da6ccbc701ddf603ddcb1bc8
billlims <davidlinxf@gmail.com>
2023-10-17 01:38:02 +1100

    using math operator

--- LeetcodeSolution/Leetcode9Solution.cs
+++ LeetcodeSolution/Leetcode9Solution.cs
@@ -8,6 +8,7 @@ namespace LeetcodeSolution
 {
     class Leetcode9Solution
     {
+        //solution one 
         public static bool IsPalindrome(int x)
         {
             //变成string （类似数组可以进行操作）
@@ -15,7 +16,7 @@ namespace LeetcodeSolution
             //左边游标
             int left = 0;
             //右边游标
-            int right = 0;
+            int right = xStr.Length -1;
 
             //当都循环结束 代表没有false 返回true
             while (left > right)
@@ -31,5 +32,26 @@ namespace LeetcodeSolution
 
             return true;
         }
+
+
+        //solution two
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+            //test reverse == x;
+            int reverse = 0;
+            int original = x;
+
+            while (x > 0)
+            {
+                int digital = x % 10;
+                reverse = reverse * 10 + digital;
+                x /= 10;
+            }
+
+            return reverse == original ? true : false;
+        }
+
     }
 }
--- LeetcodeSolution/Program.cs
+++ LeetcodeSolution/Program.cs
@@ -7,8 +7,9 @@ namespace LeetcodeSolution
         static void Main(string[] args)
         {
             Console.WriteLine("--------------quetion 9----------------------");
-            Leetcode9Solution.IsPalindrome(121);
-            Leetcode9Solution.IsPalindrome(-121);
+            //Leetcode9Solution.IsPalindrome(121);
+            //Leetcode9Solution.IsPalindrome(-121);
+            Console.WriteLine(Leetcode9Solution.IsPalindrome(0));
         }
     }
 }
