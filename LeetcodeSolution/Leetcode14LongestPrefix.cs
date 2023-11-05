7d5af2fb27ba1c11976c339a28788dd8d8529941
billlims <davidlinxf@gmail.com>
2023-11-05 13:57:54 +1100

    the solution of question 14

---
+++ LeetcodeSolution/Leetcode14LongestPrefix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolution
+{
+    class Leetcode14LongestPrefix
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string res = "";
+            int len = strs.Length;
+            int count = 3;
+            for (int i = 0; i < len; i++)
+            {
+                //获取第一个数组元素的第一个元素
+                char findLetter = strs[0][i];
+                for (int j = 1; j < strs[0].Length j++)
+                {
+                    if(strs[j][i] != findLetter)
+                    {
+                        count--;
+                        break;
+                    }
+                }
+                if(count == 3)
+                {
+                    res += findLetter;
+                }
+            }
+            return res;
+
+        }
+    }
+}
--- LeetcodeSolution/Program.cs
+++ LeetcodeSolution/Program.cs
@@ -10,12 +10,15 @@ namespace LeetcodeSolution
             //Leetcode9Solution.IsPalindrome(121);
             //Leetcode9Solution.IsPalindrome(-121);
             Console.WriteLine(Leetcode9Solution.IsPalindrome(0));
-            int[] nums = {1, 2, 3};
+            int[] nums = { 1, 2, 3 };
             Console.WriteLine(Leetcode78.Subsets(nums));
             Console.WriteLine(Leetcode22.GenerateParenthesis(3));
             Console.WriteLine("---------------qustion 13------------------------");
             string s = "LVIII";
             Leetcode13RomanToInteger.RomanToInteger(s);
+            Console.WriteLine("-----------------quetion14-----------------------");
+            string[] strs = { "flower", "flow", "flight" };
+            Console.WriteLine(Leetcode14LongestPrefix.LongestCommonPrefix(strs)); ;
         }
     }
 }
