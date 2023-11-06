1b5301f959109e1e0e16a5bbb8d568b62b935931
billlims <davidlinxf@gmail.com>
2023-11-06 17:27:45 +1100

    the solution of leetcode quetion 27

---
+++ LeetcodeSolution/Leetcode27RemoveElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolution
+{
+    class Leetcode27RemoveElement
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            //重置新的下标
+            int newIndex = 0;
+            //循环检测不等于val得值
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    
+                    nums[newIndex] = nums[i];
+                    newIndex++;
+                }
+            }
+            return newIndex;
+        }
+    }
+}
