d3372c318d9289cda0fd3ad12c243b0e9fb82dc1
billlims <davidlinxf@gmail.com>
2023-11-29 16:03:20 +1100

    solution for leetcode 83 remove duplicate sorted list

---
+++ LeetcodeSolution/Leetcode83RemoveDup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+namespace LeetcodeSolution
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Leetcode83RemoveDup
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode cur = head;
+
+            while (cur != null && cur.next != null)
+            {
+
+                if (cur.val == cur.next.val)
+                {
+                    cur.next = cur.next.next;
+                }
+                else
+                {
+                    cur = cur.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
