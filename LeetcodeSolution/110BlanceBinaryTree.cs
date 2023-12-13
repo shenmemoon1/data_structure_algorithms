42d2bd624f6bb768b79d196fff00266bf9fd68be
billlims <davidlinxf@gmail.com>
2023-12-13 15:28:43 +1100

    solution for 110 balance bianry tree

---
+++ LeetcodeSolution/110BlanceBinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolution
+{
+    class _110BlanceBinaryTree
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null) return true;
+            if (Math.Abs(Height(root.left) - Height(root.right)) > 1) return false;
+            return IsBalanced(root.left) && IsBalanced(root.right);
+        }
+
+        public int Height(TreeNode node)
+        {
+            if (node == null) return 0;
+            return Math.Max(Height(node.left), Height(node.right)) + 1;
+        }
+    }
+}
