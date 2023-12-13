09a5d28cf6b6315655c4e74fdc425b0a96cebedc
billlims <davidlinxf@gmail.com>
2023-12-13 15:56:57 +1100

    solution for 111 min binary tree

---
+++ LeetcodeSolution/111LeetcodeMinBinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolution
+{
+    class _111LeetcodeMinBinaryTree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            List<int> depths = new List<int>();
+            Callback(root, 1, depths);
+            return depths.Min();
+        }
+
+        public void Callback(TreeNode node, int dep, List<int> depths)
+        {
+            if (node.left == null && node.right == null)
+            {
+                depths.Add(dep);
+                return;
+            }
+
+            if (node.left != null)
+            {
+                Callback(node.left, dep + 1, depths);
+            }
+            if (node.right != null)
+            {
+                Callback(node.right, dep + 1, depths);
+            }
+        }
+
+    }
+}
