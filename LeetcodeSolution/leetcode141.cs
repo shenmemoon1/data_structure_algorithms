05c5184c94ced23d169d0d85ca6bf37413d0ebc0
billlims <davidlinxf@gmail.com>
2024-01-19 22:31:42 +1100

    solution for leetcode 141

---
+++ LeetcodeSolution/leetcode141.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolution
+{
+    class leetcode141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            HashSet<ListNode> set = new HashSet<ListNode>();
+            ListNode curr = head;
+
+            while (curr != null)
+            {
+                if (set.Contains(curr))
+                {
+                    return true;
+                }
+                else
+                {
+                    set.Add(curr);
+                }
+
+                curr = curr.next;
+            }
+
+            return false;
+        }
+    }
+}
