d431deb55d72f44140a180c8739a0382efe54409
billlims <davidlinxf@gmail.com>
2024-01-20 23:50:38 +1100

    solution for leetcode 144

--- LeetcodeSolution/leetcode144.cs
+++ LeetcodeSolution/leetcode144.cs
@@ -8,5 +8,22 @@ namespace LeetcodeSolution
 {
     class leetcode144
     {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+            IList<int> path = new List<int>();
+            PreOrder(root, path);
+            return path;
+
+        }
+
+        public void PreOrder(TreeNode root, IList<int> list)
+        {
+            if (root == null) return;
+            // root - left - right
+            list.Add(root.val);
+            PreOrder(root.left, list);
+            PreOrder(root.right, list);
+        }
     }
 }
