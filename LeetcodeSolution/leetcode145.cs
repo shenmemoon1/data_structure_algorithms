6a360b9a5be95e932d3a2e3ea64ca9eaa8852edd
billlims <davidlinxf@gmail.com>
2024-01-22 00:10:04 +1100

    solution for leetcode 145

--- LeetcodeSolution/leetcode145.cs
+++ LeetcodeSolution/leetcode145.cs
@@ -8,5 +8,25 @@ namespace LeetcodeSolution
 {
     class leetcode145
     {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+
+            IList<int> posList = new List<int>();
+            PostorderTra(root, posList);
+            return posList;
+        }
+
+        public void PostorderTra(TreeNode node, IList<int> path)
+        {
+
+            //end condition
+            if (node == null) return;
+            //left - right -root
+            PostorderTra(node.left, path);
+            PostorderTra(node.right, path);
+
+            path.Add(node.val);
+        }
     }
 }
