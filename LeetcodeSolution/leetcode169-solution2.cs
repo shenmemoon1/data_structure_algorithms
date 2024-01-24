638c633bc8c629892edd86cc8e298757cab96c7a
billlims <davidlinxf@gmail.com>
2024-01-24 20:32:49 +1100

    solution 3 for leetcode 169

---
+++ LeetcodeSolution/leetcode169-solution2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolution
+{
+    class leetcode169_solution2
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    dic[nums[i]]++;
+                }
+                else
+                {
+                    dic.Add(nums[i], 0);
+                }
+            }
+            return FindMaxValue(dic);
+        }
+        static int FindMaxValue(Dictionary<int, int> dictionary)
+        {
+            if (dictionary == null || dictionary.Count == 0)
+            {
+                //if dictionary is empty
+                throw new InvalidOperationException("dictionary empty");
+            }
+
+            // get max key and value
+            int maxKey = int.MinValue;
+            int maxValue = int.MinValue;
+
+            // loop all entry and return max key
+            foreach (var entry in dictionary)
+            {
+                if (entry.Value > maxValue)
+                {
+                    maxKey = entry.Key;
+                    maxValue = entry.Value;
+                }
+            }
+
+            return maxKey;
+        }
+    }
+}
